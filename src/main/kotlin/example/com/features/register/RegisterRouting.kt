19702085a66ba73c9ef2351b99caeb89a4ecc3db
6akashi <semen.yby@gmail.com>
2024-07-10 11:24:56 +0300

    update RegisterRouting.kt and Serialization.kt

--- src/main/kotlin/example/com/features/register/RegisterRouting.kt
+++ src/main/kotlin/example/com/features/register/RegisterRouting.kt
@@ -1,12 +1,15 @@
 package example.com.features.register
 
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.routing.*
 
 fun Application.configureRegisterRouting(){
     routing {
         post("/register"){
-
+            val registerController = RegisterController(call)
+            registerController.registerNewUser()
+            val remote = call.receive(RegisterRecieveRemote::class)
         }
     }
 }
